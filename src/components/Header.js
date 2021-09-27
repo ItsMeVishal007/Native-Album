eeb082c19c17876fe40362e73ec9409366d634d0
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-27 12:55:07 +0530

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return <Text>Album</Text>;
+}
+
+const styles = StyleSheet.create({});
